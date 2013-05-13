7e9528f50b01ade57c3f3ac3a6123ca6a3a3214b
Raúl Moreno <raulmorenogaldon@gmail.com>
2013-05-13 12:32:16 +0200

    Añadidos scripts para ejecutar el recalibrado del bam humano de prueba

---
+++ ex_human_ph1.sh
@@ -0,0 +1,1 @@
+bin/recal-debug -1 -C 100 -R "../bams/human_bams/dna_compression.bin" -I "../bams/human_bams/test.bam" -d "humandata.data" -i "humandata.info"
---
+++ ex_human_ph2.sh
@@ -0,0 +1,1 @@
+bin/recal-debug -2 -C 90 -R "../bams/human_bams/dna_compression.bin" -I "../bams/human_bams/test.bam" -c "../bams/recal_output/humanoutputgatk.bam" -o "../bams/recal_output/humanoutputrecal.bam" -d "humandata.data" -i "humandata.info"
