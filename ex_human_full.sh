19b69c9778c585a6931d4abb6538646078e12836
Raúl Moreno <raulmorenogaldon@gmail.com>
2013-05-14 11:47:58 +0200

    Añadido script para recalibrar por completo el bam humano

---
+++ ex_human_full.sh
@@ -0,0 +1,1 @@
+bin/recal-debug -C 100 -R "../bams/human_bams/dna_compression.bin" -I "../bams/human_bams/test.bam" -o "../bams/recal_output/humanoutputrecal.bam" -d "humandata.data" -i "humandata.info"
