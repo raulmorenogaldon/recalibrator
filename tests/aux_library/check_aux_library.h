24189751ded3102476aad198f802d9c6fdf4693a
Raúl Moreno <raulmorenogaldon@gmail.com>
2013-07-01 12:28:49 +0200

    First testing implementation, with check.

---
+++ tests/aux_library/check_aux_library.h
@@ -0,0 +1,12 @@
+
+#ifndef CHECK_AUX_LIBRARY_H_
+#define CHECK_AUX_LIBRARY_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <check.h>
+#include "../../src/common/common.h"
+#include "../../src/common/recal_config.h"
+#include "../../src/aux_library/aux_library.h"
+
+#endif /* CHECK_AUX_LIBRARY_H_ */
