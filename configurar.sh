7dc39d773ce8176afd87d367d03c876645e83a16
Raúl Moreno <raulmorenogaldon@gmail.com>
2013-07-11 13:07:44 +0200

    Minor changes to configurar script.

--- configurar.sh
+++ configurar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#CFLAGS="-O0 -g -std=c99 -Wall -D_REENTRANT -D_XOPEN_SOURCE=500 -fPIC"		\
+CFLAGS="-O3 -std=c99 -Wall -D_REENTRANT -D_XOPEN_SOURCE=500 -fPIC"		\
 ./configure 	--with-samtools=/home/rmoreno/ext/samtools-0.1.18/ 	\
 		--with-compbio=/home/rmoreno/workspace/compbio-hpg/	\
 		--prefix=/home/rmoreno/recalibrator
